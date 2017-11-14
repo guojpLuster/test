28ec0760735a6bd9918f6bf6da451c2e8685efcd
guojpLuster <guojp@goldlokedu.cn>
2017-11-14 17:02:53 +0800

    this is a test

---
+++ index.php
@@ -0,0 +1,4 @@
+<form action="post.php" method="post">
+	<input type="text" name="name"/>
+	<input type="submit" value="submit"/>
+</form>
---
+++ post.php
@@ -0,0 +1,5 @@
+<?php
+error_reporting(0);
+    $name = $_POST['name'];
+    echo $name;
+?>
